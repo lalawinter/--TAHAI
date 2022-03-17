8a2874932e145c48bc37a4275b88ccfd82134250
lalawinter <49677789+lalawinter@users.noreply.github.com>
2022-03-17 10:47:39 +0800

    字典树

---
+++ 字典树.cpp
@@ -0,0 +1,39 @@
+class Tries {
+private:
+    bool flag;
+    Tries* next[26] = {nullptr};
+public:
+    Tries {};
+
+    void insert(string& word) {
+        Tries* node = this;
+        for(int i = 0; i < word.size(); i++) {
+            char c = word[i];
+            if(node->next[c-'a'] == nullptr) {
+                node->next[c-'a'] = new Tries();
+            }
+            node = node->next[c-'a'];
+        }
+        node->flag = true;
+    }
+
+    bool search(string& word) {
+        Tries* node = this;
+        for(int i = 0; i < word.size(); i++) {
+            char c = word[i];
+            if(node->next[c-'a' == nullptr]) return false;
+            node = node->next[c-'a'];
+        }
+        return node->flag;
+    }
+
+    bool startWith(string& prefix) {
+        Tries* node = this;
+        for(int i = 0; i < prefix.size(); i++) {
+            char c = prefix[i];
+            if(node->next[c-'a'] == nullptr) return false;
+            node = node->next[c-'a'];
+        }
+        return true;
+    }
+}
